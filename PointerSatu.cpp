913d484388e87f75a4d83daf8147976455b283d8
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2025-05-06 11:08:52 +0700

    add reference to mahasiswa object and update nim value in main function

--- PointerSatu.cpp
+++ PointerSatu.cpp
@@ -13,5 +13,9 @@ int main(){
     mahasiswa mhs{1}; // object mhs
     mhs.showNim(); // Member Access Operator
 
+    mahasiswa &refMhs = mhs; // pointer Reference refMhs
+    refMhs.nim = 2; // Memberi Access Operator
+    mhs.showNim();
+
    
 }
